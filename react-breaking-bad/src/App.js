2357da8e96a0629ff69c80526fc7abc972504ba6
Dell Home <pon.felipon@gmail.com>
2021-06-27 22:14:20 -0700

    quitando el boton por mientras

--- react-breaking-bad/src/App.js
+++ react-breaking-bad/src/App.js
@@ -1,18 +1,18 @@
 import { useState, useEffect} from "react";
-import styled from '@emotion/styled'
+// import styled from '@emotion/styled'
 import Quote from './components/Quote'
 
-const Button = styled.button`
-  background-image: linear-gradient(to left, #007d35 0%, #007d35 40%, #0f574e 100%);
-  background-size: 300px;
-  font-family: Arial, Helvetica, sans-serif;
-  color: #fff;
-  margin-top: 3rem;
-  padding: 1rem, 3rem;
-  font-size: 2rem;
-  border: 2px solid black;
-	cursor: pointer
-  `
+// const Button = styled.button`
+//   background-image: linear-gradient(to left, #007d35 0%, #007d35 40%, #0f574e 100%);
+//   background-size: 300px;
+//   font-family: Arial, Helvetica, sans-serif;
+//   color: #fff;
+//   margin-top: 3rem;
+//   padding: 1rem, 3rem;
+//   font-size: 2rem;
+//   border: 2px solid black;
+// 	cursor: pointer
+//   `
 
   // const consultarAPI = () => {
   //   const request = fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');

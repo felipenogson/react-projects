a786a22c81ca7b3964de418250fa9cbe194c9351
Dell Home <pon.felipon@gmail.com>
2021-08-10 17:03:40 -0700

    inicio del proyecto react-images

---
+++ react-images/src/App.js
@@ -0,0 +1,30 @@
+
+function App() {
+  return (
+
+    <div className="container">
+
+      <div class="p-5 bg-light mt-5">
+        <div class="container">
+          <h1 class="display-3">React-Images</h1>
+          <p class="lead">Image Search Engine</p>
+          <hr class="my-2" />
+            <form class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  </form>
+          <p class="lead">
+            <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+          </p>
+        </div>
+      </div>
+
+
+
+
+    </div>
+
+  );
+}
+
+export default App;
